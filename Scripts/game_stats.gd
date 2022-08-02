b8ada32dce56d3d6e73b15aad6c6e8bfea00042f
UltraFormula1 <100097049+UltraFormula1@users.noreply.github.com>
2022-08-02 13:25:56 +1200

    Day 10
    Worked more on checkpoint and added forced reset key.

--- Scripts/game_stats.gd
+++ Scripts/game_stats.gd
@@ -1,11 +1,29 @@
 extends Node
 
+onready var game_start_time = OS.get_ticks_msec()
+var current_spawn = null
 
-func _ready():
-	$AnimationPlayer.play("Checkpoint")
+func reset():
+	current_spawn = null
+	get_tree().reload_current_scene()
+	game_start_time = OS.get_ticks_msec()
 
-func _process(delta):
-	if GameStats.get_spawn() != self:
-		$AnimationPlayer.play("checkpoint")
+func check_reset():
+	if current_spawn == null:
+		reset()
+	else:
+		return false
+
+func set_spawn(spawn):
+	current_spawn = spawn
+
+func get_spawn():
+	return current_spawn
+
+#func _ready():
+	#pass
+
+#func get_time():
+	#pass
 	
 
