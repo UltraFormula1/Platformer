c1dede000b155d11c7057b1c8b95231da134f18a
UltraFormula1 <100097049+UltraFormula1@users.noreply.github.com>
2022-08-29 15:02:22 +1200

    Day 20
    Continued on menu

--- Scenes/Title.gd
+++ Scenes/Title.gd
@@ -3,3 +3,15 @@ extends Control
 
 func _ready():
 	SoundPlayer.change_music("menu")
+
+
+func _on_Options_pressed():
+	$AnimationPlayer.play("MoveToOptions")
+
+
+func _on_Back_pressed():
+	$AnimationPlayer.play("BackToMenu")
+
+
+func _on_HSlider_value_changed(value):
+	SoundPlayer.change_music.volume(value)
--- Scripts/sound_player.gd
+++ Scripts/sound_player.gd
@@ -1,6 +1,6 @@
 extends Node
 
-onready var music = AudioStreamPlayer.new()
+onready var music_player = AudioStreamPlayer.new()
 
 var music_tracks = {
 	"main" : "res://Sound Effects/onlymp3.to - Mario Circuit - Super Mario Kart Music Extended-52piB79kiI4-192k-1660090254971.mp3",
@@ -18,22 +18,23 @@ var music_db = 1
 var sound_db = 1
 
 func change_music_db(val):
-	music_db = linear2db(val)
+		music_db = linear2db(val)
+		music_player.volume_db = music_db
 	
 func change_sound_db(val):
 	sound_db = linear2db(val)
 
 func _ready():
 	#music.stream = load(music_tracks["main"])
-	add_child(music)
-	music.play()
-	print(music.stream)
+	add_child(music_player)
+	music_player.play()
+	print(music_player.stream)
 	print("playing song")
 
 func change_music(track):
 	if track in music_tracks:
-		music.stream = load(music_tracks[track])
-		music.play()
+		music_player.stream = load(music_tracks[track])
+		music_player.play()
 	
 
 
@@ -42,5 +43,6 @@ func play_sound_effect(sfx):
 	sound.stream = load(sound_effects[sfx])
 	add_child(sound)
 	sound.play()
+	sound.volume_db = sound_db
 	yield(sound,"finished")
 	sound.queue_free()
