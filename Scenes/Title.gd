5ffdd20065ef6d4333ef69e91674d3b521b45803
UltraFormula1 <100097049+UltraFormula1@users.noreply.github.com>
2022-09-01 11:08:53 +1200

    Day 22
    Redesigned stage, finished menu

--- Scenes/Title.gd
+++ Scenes/Title.gd
@@ -19,3 +19,15 @@ func _on_HSlider_value_changed(value):
 
 func _on_SFX_value_changed(value):
 	SoundPlayer.change_sound_db(value)
+
+
+func _on_CheckBox_toggled(button_pressed):
+	OS.window_fullscreen = !OS.window_fullscreen
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Scenes/World.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
--- Scenes/World.gd
+++ Scenes/World.gd
@@ -8,3 +8,5 @@ func _process(delta):
 	if Input.is_action_just_pressed("reset"):
 		GameStats.reset()
 	$CanvasLayer/Control/Label.text = GameStats.get_time()
+	if Input.is_action_just_pressed("menu"):
+		get_tree().change_scene("res://Scenes/Title.tscn")
